f668b5b407dc9208a642a05bbbd62207b94ad06d
Misbah Omar <misbah.omar@planckepoch.com>
2023-03-03 12:34:53 +0530

    basic structure and layout v2

--- src/app/existing-contacts/existing-contacts.component.ts
+++ src/app/existing-contacts/existing-contacts.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./existing-contacts.component.css']
 })
 export class ExistingContactsComponent {
-
+  contacts = [
+    { id: 1, name: 'Kelly Williamson', position: 'General Manager', email: 'kelly.williamson@company.com', phone: '+47 589 55 458', Image: '../../assets/girl1.jpg'},
+    { id: 2, name: 'Brandon Hoffman', position: 'General Manager', email: 'brandon.hoffman@company.com', phone: '+47 589 55 458', Image: '../../assets/boy1.jpg'},
+    { id: 3, name: 'Eliza Miller', position: 'General Manager', email: 'eliza.miller@company.com', phone: '+47 589 55 458', Image: '../../assets/girl2.jpg'}
+  ];
 }
